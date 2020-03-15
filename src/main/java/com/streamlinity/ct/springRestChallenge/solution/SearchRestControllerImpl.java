2651574ba6fe48702d8de9b07dc6d8ea8ec71175
Baicong Gu <gubaicong1993@gmail.com>
2020-03-14 18:59:05 -0700

    Interview code question solution

--- src/main/java/com/streamlinity/ct/springRestChallenge/solution/SearchRestControllerImpl.java
+++ src/main/java/com/streamlinity/ct/springRestChallenge/solution/SearchRestControllerImpl.java
@@ -1,7 +1,15 @@
 package com.streamlinity.ct.springRestChallenge.solution;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.streamlinity.ct.springRestChallenge.api.Item;
 
 /*
  * This controller needs to expose the following rest endpoints.  You need to fill in the implementation here
@@ -16,4 +24,29 @@ import org.springframework.web.bind.annotation.RestController;
 @Profile("default")
 @RestController
 public class SearchRestControllerImpl {
+	
+	@Autowired
+	SearchSvcImpl searchSvcImpl;
+	
+	@GetMapping("/item")
+	@ResponseBody
+	public List<Item> getAllItems() {
+		
+		searchSvcImpl.init("itemPrices.json");
+		searchSvcImpl.getItems();
+		
+		return searchSvcImpl.getItemList();
+	}
+	
+	@GetMapping("/item?{category}")
+	@ResponseBody
+	public List<Item> getItemsForCategory(@PathVariable("category") String category) {
+		return searchSvcImpl.getItems(category);
+	}
+	
+	@GetMapping("/item?{itemShortName}")
+	@ResponseBody
+	public List<Item> getItemsForItemShortName(@PathVariable("itemShortName") String itemShortName) {
+		return searchSvcImpl.getItems(itemShortName);
+	}
 }
--- src/main/java/com/streamlinity/ct/springRestChallenge/solution/SearchSvcImpl.java
+++ src/main/java/com/streamlinity/ct/springRestChallenge/solution/SearchSvcImpl.java
@@ -1,10 +1,21 @@
 package com.streamlinity.ct.springRestChallenge.solution;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.streamlinity.ct.springRestChallenge.api.Item;
 import com.streamlinity.ct.springRestChallenge.api.SearchSvcInterface;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 /*
  * Provide your implementation of the SearchSvcImpl here.
@@ -17,28 +28,78 @@ import java.util.List;
  */
 
 public class SearchSvcImpl implements SearchSvcInterface {
-    @Override
-    public void init(String itemPriceJsonFileName) {
 
-    }
+	private JSONArray allItemsJson;
+	private List<Item> itemList;
 
-    @Override
-    public void init(File itemPriceJsonFile) {
+	@Override
+	public void init(String itemPriceJsonFileName) {
 
-    }
+		File inputFile = new File(getClass().getClassLoader().getResource(itemPriceJsonFileName).getFile());
 
-    @Override
-    public List<Item> getItems() {
-        return null;
-    }
+		try {
+			InputStream is = new FileInputStream(inputFile.getPath());
+			String allItemsString = IOUtils.toString(is);
+			allItemsJson = new JSONArray(allItemsString);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public List<Item> getItems(String category) {
-        return null;
-    }
+	@Override
+	public void init(File itemPriceJsonFile) {
 
-    @Override
-    public List<Item> getItem(String itemShortName) {
-        return null;
-    }
+		try {
+			InputStream is = new FileInputStream(itemPriceJsonFile.getPath());
+			String allItemsString = IOUtils.toString(is);
+			allItemsJson = new JSONArray(allItemsString);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public List<Item> getItems() {
+
+		ObjectMapper mapper = new ObjectMapper();
+		int size = allItemsJson.length();
+		itemList = new ArrayList<>();
+
+		try {
+			for (int i = 0; i < size; i++) {
+				itemList.add(mapper.readValue(allItemsJson.getJSONObject(i).toString(), Item.class));
+			}
+		} catch (JsonProcessingException | JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return itemList;
+	}
+
+	@Override
+	public List<Item> getItems(String category) {
+		List<Item> newItemListForCategory = itemList.stream().filter(item -> {
+			return item.getCategory_short_name().equals(category);
+		}).collect(Collectors.toList());
+		
+		return newItemListForCategory;
+	}
+
+	@Override
+	public List<Item> getItem(String itemShortName) {
+		List<Item> newItemListForItemShortName = itemList.stream().filter(item -> {
+			return item.getShort_name().equals(itemShortName);
+		}).collect(Collectors.toList());
+		
+		return null;
+	}
+
+	public List<Item> getItemList() {
+		return itemList;
+	}
+	
+	
 }
